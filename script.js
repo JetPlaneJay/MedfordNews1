cca8e98d0170dc1603d0987ede0dc842f99cd938
Jacob Anthony Tabares <73261191+JetPlaneJay@users.noreply.github.com>
2022-03-14 13:47:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -16,110 +16,110 @@
 //  document.querySelector('.article').style.height = '200px'
 // }
 
-// const change = document.querySelector('.article')
+// // const change = document.querySelector('.article')
 
-const change = e => {
-    const changed = document.querySelector('.article1');
-    const blah = document.querySelector('.text1');
-    const also = document.querySelector('.buttonArticle1');
-    e.stopPropagation();
-    blah.style.fontSize = '2em';
-    blah.style.visibility= 'visible';
-    also.textContent = 'Double-Click to Make Small';
-    changed.style.height = '100%';
-    changed.style.width = '200%';
-    changed.style.zIndex = '1';
-    changed.style.margin = '50%';
+// const change = e => {
+//     const changed = document.querySelector('.article1');
+//     const blah = document.querySelector('.text1');
+//     const also = document.querySelector('.buttonArticle1');
+//     e.stopPropagation();
+//     blah.style.fontSize = '2em';
+//     blah.style.visibility= 'visible';
+//     also.textContent = 'Double-Click to Make Small';
+//     changed.style.height = '100%';
+//     changed.style.width = '200%';
+//     changed.style.zIndex = '1';
+//     changed.style.margin = '50%';
 
-}
-
-
-
-const revert = () => {
-    const changed = document.querySelector('.article1');
-    const blah = document.querySelector('.text1');
-    const also = document.querySelector('.buttonArticle1');
-    also.textContent = 'Click to Enlarge';
-    blah.style.fontSize = '.005em';
-    blah.style.visibility= 'hidden';
-    changed.style.height = '105%';
-    changed.style.width = '100%';
-    changed.style.zIndex = '0';
-    changed.style.margin = '0';
-
-}
+// }
 
 
 
-const changer = e => {
-    const changed = document.querySelector('.article2');
-    const blah = document.querySelector('.text2');
-    const also = document.querySelector('.buttonArticle2');
-    e.stopPropagation();
+// const revert = () => {
+//     const changed = document.querySelector('.article1');
+//     const blah = document.querySelector('.text1');
+//     const also = document.querySelector('.buttonArticle1');
+//     also.textContent = 'Click to Enlarge';
+//     blah.style.fontSize = '.005em';
+//     blah.style.visibility= 'hidden';
+//     changed.style.height = '105%';
+//     changed.style.width = '100%';
+//     changed.style.zIndex = '0';
+//     changed.style.margin = '0';
 
-    blah.style.fontSize = '2em';
-    blah.style.visibility= 'visible';
-    also.textContent = 'Double-Click to Make Small';
-    changed.style.height = '100%';
-    changed.style.width = '200%';
-    changed.style.zIndex = '1';
-
-}
-
-const reverter = () => {
-    const changed = document.querySelector('.article2');
-    const blah = document.querySelector('.text2');
-    const also = document.querySelector('.buttonArticle2');
-    also.textContent = 'Click to Enlarge';
-    blah.style.fontSize = '.005em';
-    blah.style.visibility= 'hidden';
-    changed.style.height = '105%';
-    changed.style.width = '100%';
-    changed.style.zIndex = '0';
+// }
 
 
-}
 
-const changered = e => {
-    const changed = document.querySelector('.article3');
-    const blah = document.querySelector('.text3');
-    const also = document.querySelector('.buttonArticle3');
-    e.stopPropagation();
+// const changer = e => {
+//     const changed = document.querySelector('.article2');
+//     const blah = document.querySelector('.text2');
+//     const also = document.querySelector('.buttonArticle2');
+//     e.stopPropagation();
 
-    blah.style.fontSize = '2em';
-    blah.style.visibility= 'visible';
-    also.textContent = 'Double-Click to Make Small';
-    changed.style.height = '100%';
-    changed.style.width = '200%';
-    changed.style.zIndex = '1';
+//     blah.style.fontSize = '2em';
+//     blah.style.visibility= 'visible';
+//     also.textContent = 'Double-Click to Make Small';
+//     changed.style.height = '100%';
+//     changed.style.width = '200%';
+//     changed.style.zIndex = '1';
 
-}
+// }
 
-const revertered = () => {
-    const changed = document.querySelector('.article3');
-    const blah = document.querySelector('.text3');
-    const also = document.querySelector('.buttonArticle3');
-    also.textContent = 'Click to Enlarge';
-    blah.style.fontSize = '.005em';
-    blah.style.visibility= 'hidden';
-    changed.style.height = '105%';
-    changed.style.width = '100%';
-    changed.style.zIndex = '0';
+// const reverter = () => {
+//     const changed = document.querySelector('.article2');
+//     const blah = document.querySelector('.text2');
+//     const also = document.querySelector('.buttonArticle2');
+//     also.textContent = 'Click to Enlarge';
+//     blah.style.fontSize = '.005em';
+//     blah.style.visibility= 'hidden';
+//     changed.style.height = '105%';
+//     changed.style.width = '100%';
+//     changed.style.zIndex = '0';
 
 
-}
+// }
 
-const button = document.querySelector('.buttonArticle1');
-button.addEventListener('click', change);
-button.addEventListener('dblclick', revert);
+// const changered = e => {
+//     const changed = document.querySelector('.article3');
+//     const blah = document.querySelector('.text3');
+//     const also = document.querySelector('.buttonArticle3');
+//     e.stopPropagation();
 
-const random = document.querySelector('.buttonArticle2');
-random.addEventListener('click', changer);
-random.addEventListener('dblclick', reverter);
+//     blah.style.fontSize = '2em';
+//     blah.style.visibility= 'visible';
+//     also.textContent = 'Double-Click to Make Small';
+//     changed.style.height = '100%';
+//     changed.style.width = '200%';
+//     changed.style.zIndex = '1';
 
-const hello = document.querySelector('.buttonArticle3');
-hello.addEventListener('click', changered);
-hello.addEventListener('dblclick', revertered);
+// }
+
+// const revertered = () => {
+//     const changed = document.querySelector('.article3');
+//     const blah = document.querySelector('.text3');
+//     const also = document.querySelector('.buttonArticle3');
+//     also.textContent = 'Click to Enlarge';
+//     blah.style.fontSize = '.005em';
+//     blah.style.visibility= 'hidden';
+//     changed.style.height = '105%';
+//     changed.style.width = '100%';
+//     changed.style.zIndex = '0';
+
+
+// }
+
+// const button = document.querySelector('.buttonArticle1');
+// button.addEventListener('click', change);
+// button.addEventListener('dblclick', revert);
+
+// const random = document.querySelector('.buttonArticle2');
+// random.addEventListener('click', changer);
+// random.addEventListener('dblclick', reverter);
+
+// const hello = document.querySelector('.buttonArticle3');
+// hello.addEventListener('click', changered);
+// hello.addEventListener('dblclick', revertered);
 
 // function blah() {
 //     change.style.height = '160px';
