505228e1a13179a4a9e07d3b030d3bfdcb2b9569
Jacob Anthony Tabares <73261191+JetPlaneJay@users.noreply.github.com>
2022-03-19 11:58:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,110 +1,93 @@
 
-
-
-//  const big = document.querySelector('.article');
-
-// function bigger() {
-//    big.style.width = 100px;
-
-// }
-
-// function mouseOver() {
-//     document.querySelector('.article').style.height = "400px";
-//  }
-
-// function blah(){
-//  document.querySelector('.article').style.height = '200px'
-// }
-
-// // const change = document.querySelector('.article')
-
 // const change = e => {
-//     const changed = document.querySelector('.article1');
+//     const changed = document.querySelector('.article');
 //     const blah = document.querySelector('.text1');
 //     const also = document.querySelector('.buttonArticle1');
 //     e.stopPropagation();
 //     blah.style.fontSize = '2em';
 //     blah.style.visibility= 'visible';
 //     also.textContent = 'Double-Click to Make Small';
-//     changed.style.height = '100%';
-//     changed.style.width = '200%';
-//     changed.style.zIndex = '1';
-//     changed.style.margin = '50%';
+//     // changed.style.height = '100%';
+//     // changed.style.width = '200%';
+//   changed.style.zIndex = '1';
+//     // changed.style.margin = '50%';
+
 
 // }
 
 
 
 // const revert = () => {
-//     const changed = document.querySelector('.article1');
+//     // const changed = document.querySelector('.article1');
 //     const blah = document.querySelector('.text1');
 //     const also = document.querySelector('.buttonArticle1');
 //     also.textContent = 'Click to Enlarge';
 //     blah.style.fontSize = '.005em';
 //     blah.style.visibility= 'hidden';
-//     changed.style.height = '105%';
-//     changed.style.width = '100%';
-//     changed.style.zIndex = '0';
-//     changed.style.margin = '0';
+//     // changed.style.height = '105%';
+//     // changed.style.width = '100%';
+//     // changed.style.zIndex = '0';
+//     // changed.style.margin = '0';
+
 
 // }
 
 
 
 // const changer = e => {
-//     const changed = document.querySelector('.article2');
+//     const changed = document.querySelector('.article');
 //     const blah = document.querySelector('.text2');
 //     const also = document.querySelector('.buttonArticle2');
 //     e.stopPropagation();
-
 //     blah.style.fontSize = '2em';
 //     blah.style.visibility= 'visible';
 //     also.textContent = 'Double-Click to Make Small';
-//     changed.style.height = '100%';
-//     changed.style.width = '200%';
-//     changed.style.zIndex = '1';
+//     // changed.style.height = '100%';
+//     // changed.style.width = '200%';
+//   changed.style.zIndex = '1';
+//     // changed.style.margin = '50%';
 
 // }
 
 // const reverter = () => {
-//     const changed = document.querySelector('.article2');
+//     // const changed = document.querySelector('.article1');
 //     const blah = document.querySelector('.text2');
 //     const also = document.querySelector('.buttonArticle2');
 //     also.textContent = 'Click to Enlarge';
 //     blah.style.fontSize = '.005em';
 //     blah.style.visibility= 'hidden';
-//     changed.style.height = '105%';
-//     changed.style.width = '100%';
-//     changed.style.zIndex = '0';
-
+//     // changed.style.height = '105%';
+//     // changed.style.width = '100%';
+//     // changed.style.zIndex = '0';
+//     // changed.style.margin = '0';
 
 // }
 
 // const changered = e => {
-//     const changed = document.querySelector('.article3');
+//     const changed = document.querySelector('.article');
 //     const blah = document.querySelector('.text3');
 //     const also = document.querySelector('.buttonArticle3');
 //     e.stopPropagation();
-
 //     blah.style.fontSize = '2em';
 //     blah.style.visibility= 'visible';
 //     also.textContent = 'Double-Click to Make Small';
-//     changed.style.height = '100%';
-//     changed.style.width = '200%';
+//     // changed.style.height = '100%';
+//     // changed.style.width = '200%';
 //     changed.style.zIndex = '1';
+
 
 // }
 
 // const revertered = () => {
-//     const changed = document.querySelector('.article3');
+//     const changed = document.querySelector('.article');
 //     const blah = document.querySelector('.text3');
 //     const also = document.querySelector('.buttonArticle3');
 //     also.textContent = 'Click to Enlarge';
 //     blah.style.fontSize = '.005em';
 //     blah.style.visibility= 'hidden';
-//     changed.style.height = '105%';
-//     changed.style.width = '100%';
-//     changed.style.zIndex = '0';
+//     // changed.style.height = '105%';
+//     // changed.style.width = '100%';
+//     // changed.style.zIndex = '0';
 
 
 // }
@@ -121,21 +104,59 @@
 // hello.addEventListener('click', changered);
 // hello.addEventListener('dblclick', revertered);
 
-// function blah() {
-//     change.style.height = '160px';
-//     change.style.width = '160px';
-//     change.style.zIndex = '1';
 
-//     }
+function introduction() {
+    let text;
+    let person = prompt("Please enter your name:", "'Arry Pawtah");
+    if (person == null || person == "") {
+        text = "Okay then...the mystery continues!";
+    } else {
+        text = "Hello " + person + "! Welcome to Medford's local storybook..."
+    }
+    document.querySelector('.intro').innerHTML = text;
+}
+
+const hey = e => {
+    const visible = document.querySelector(".article2text");
+    visible.style.visibility= "visible";
+    visible.style.fontSize = "1em";
+};
+
+const buttonShow = document.querySelector(".articleButton1")
+buttonShow.addEventListener('click', hey);
+
+
+const yo = e => {
+    const visible = document.querySelector(".article3text");
+    visible.style.visibility= "visible";
+    visible.style.fontSize = "1em";
+};
+
+const buttonShow1 = document.querySelector(".articleButton2")
+buttonShow1.addEventListener('click', yo)
 
 
 
-//     change.addEventListener('click', )
 
 
-// function close() {
+const ya = (e) => {
+    const invisible =document.querySelector(".article2text")
+    invisible.style.visibility = "hidden";
+    invisible.style.fontSize = ".01em"
+}
 
-// }
-// <button type="button" onClick = "document.querySelector('.article').style.height = '600px';">Click Me!</button>
+const buttonHide = document.querySelector(".articleButton1Close")
+buttonHide.addEventListener('click', ya)
 
 
+
+
+
+const woo = (e) => {
+    const invisible =document.querySelector(".article3text")
+    invisible.style.visibility = "hidden";
+    invisible.style.fontSize = ".01em"
+}
+
+const buttonHide2 = document.querySelector(".articleButton2Close")
+buttonHide2.addEventListener('click', woo)
